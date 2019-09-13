0c55ff9ef5bd210fdf25365534f6f5ef9a676455
Kritika Garg <kgarg001@odu.edu>
2019-09-12 20:45:48 -0400

    Update echoserver.py

--- echoserver.py
+++ echoserver.py
@@ -5,9 +5,11 @@ HOST= '0.0.0.0'
 PORT= 80
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+	s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 	s.bind((HOST,PORT))
 	s.listen()
-	#print("Listening on " + HOST + ":" + str(PORT))
+	print("Listening on " + HOST + ":" + str(PORT))
+	s.setblocking(False)
 
 	client_host,client_port = s.accept()
 	with client_host:
